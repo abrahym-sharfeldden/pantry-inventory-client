7c8ae6db52bb951adeb2b36a2bbbc5ef4093bd16
Abrahym Sharf Eldden <asharfeldden@gmail.com>
2022-02-24 16:51:11 -0500

    Configuring travis-ci for vps

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+set -xe
+
+if [ $TRAVIS_BRANCH == 'master' ] ; then
+  eval "$(ssh-agent -s)"
+  ssh-add ~/.ssh/id_rsa
+
+  rsync -a build/* travis@161.35.11.166:/var/www/pantry.abrahym.dev/html
+else
+  echo "Not deploying, since the branch isn't master."
+fi
