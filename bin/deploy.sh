32d1bcad3f3e2890f8860917686eaa3abd3bd809
Abrahym Sharf Eldden <asharfeldden@gmail.com>
2022-02-24 17:13:06 -0500

    Updated deploy commands

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,7 +4,9 @@ if [ $TRAVIS_BRANCH == 'master' ] ; then
   eval "$(ssh-agent -s)"
   ssh-add ~/.ssh/id_rsa
 
-  rsync -a build/* travis@161.35.11.166:/var/www/pantry.abrahym.dev/html
+  # rsync -a build/* travis@161.35.11.166:/var/www/pantry.abrahym.dev/html
+
+  cp -r build/* /var/www/pantry.abrahym.dev/html
 else
   echo "Not deploying, since the branch isn't master."
 fi
