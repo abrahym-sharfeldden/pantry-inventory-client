ceaf987ae3b1fdf350f5eae3bba8847f24d1e25c
Abrahym Sharf Eldden <asharfeldden@gmail.com>
2022-02-24 17:29:07 -0500

    .

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,7 +4,7 @@ if [ $TRAVIS_BRANCH == 'master' ] ; then
   eval "$(ssh-agent -s)"
   ssh-add ~/.ssh/id_rsa
 
-  rsync -a build/* travis@161.35.11.166:/var/www/pantry.abrahym.dev/html
+  rsync build/* travis@161.35.11.166:/var/www/pantry.abrahym.dev/html
 
   # cp -r build/* /var/www/pantry.abrahym.dev/html
 else
