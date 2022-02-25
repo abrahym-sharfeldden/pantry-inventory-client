6d6a520c82f2bcbf3e3f4d937bbffd19135d4ee5
Abrahym Sharf Eldden <asharfeldden@gmail.com>
2022-02-25 08:59:04 -0500

    Attempting to secure travis variables

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,9 +1,12 @@
 set -xe
 
 if [ $TRAVIS_BRANCH == 'master' ] ; then
-  eval "$(ssh-agent -s)"
+  # before_deploy
+  mkdir -p ${TRAVIS_HOME}/.ssh
+  ssh-keyscan -t $TRAVIS_SSH_KEY_TYPES -H $HOST_IP 2>&1 | tee -a ${TRAVIS_HOME}/.ssh/known_hosts
   ssh-add ~/.ssh/id_rsa
 
+  # deploy
   rsync -avz -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress build/* travis@$HOST_IP:$HOST_STATIC_PATH
 else
   echo "Not deploying, since the branch isn't master."
