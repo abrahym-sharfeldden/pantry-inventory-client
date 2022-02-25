22ab41c2fab1a70275a3ee958e03faf2b0aeb1f3
Abrahym Sharf Eldden <asharfeldden@gmail.com>
2022-02-25 09:53:45 -0500

    .

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,7 +3,7 @@ set -xe
 if [ $TRAVIS_BRANCH == 'master' ] ; then
   ssh-add ~/.ssh/id_rsa
 
-  rsync -avz -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress build/* travis@$HOST_IP:$HOST_STATIC_PATH
+  rsync -avz -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress build/* travis@abrahym.dev:$HOST_STATIC_PATH
 else
   echo "Not deploying, since the branch isn't master."
 fi
