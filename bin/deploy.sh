bf84233926cc11e75645e3a0588780e966d103bf
Abrahym Sharf Eldden <asharfeldden@gmail.com>
2022-02-25 09:45:41 -0500

    Reverting to working travis deployment

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,7 +3,6 @@ set -xe
 if [ $TRAVIS_BRANCH == 'master' ] ; then
   ssh-add ~/.ssh/id_rsa
 
-  # deploy
   rsync -avz -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress build/* travis@$HOST_IP:$HOST_STATIC_PATH
 else
   echo "Not deploying, since the branch isn't master."
